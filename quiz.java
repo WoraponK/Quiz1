877ac5c13495fc1a4f155916bd7129dbb7ebb5d6
WoraponK <worapon.klabsri@gmail.com>
2023-07-13 10:23:15 +0700

    update

--- quiz.java
+++ quiz.java
@@ -1,18 +1,50 @@
+import java.util.Scanner;
+
 public class quiz {
     public static void main(String[] args) {
-        
+        student s1 = new student(1, "Worapon", 2022, 3.67);
+        s1.showDetails();
+        s1.updateStudentInfo();
+        s1.showDetails();
     }
 }
 
 class student {
-    Integer ID, addmissionYear;
+    int ID, admissionYear;
     String Name;
-    Float GPA;
-
-    student(Integer ID, String Name, Integer addmissionYear, Float GPA) {
+    double GPA;
+    
+    public student(int ID, String Name, int admissionYear, double GPA) {
         this.ID = ID;
         this.Name = Name;
-        this.addmissionYear = addmissionYear;
+        this.admissionYear = admissionYear;
         this.GPA = GPA;
     }
-}
+
+    public void updateStudentInfo() {
+        Scanner keyboard = new Scanner(System.in);
+    
+        System.out.print("-------- Enter New ID: ");
+        ID = keyboard.nextInt();
+
+        System.out.print("-------- Enter New Name: ");
+        Name = keyboard.next();
+
+        System.out.print("-------- Enter New Admission Year: ");
+        admissionYear = keyboard.nextInt();
+
+        System.out.print("-------- Enter New GPA: ");
+        GPA = keyboard.nextDouble();
+
+        System.out.print("-------- Updated Successfully!\n");
+        keyboard.close();
+    }
+
+    public void showDetails() {
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + Name);
+        System.out.println("Admission Year: " + admissionYear);
+        System.out.println("GPA: " + GPA);
+    }
+
+};
